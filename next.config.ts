dd543f3bb9dcd3766727661f70fe6a9ec6dfc0ee
son <handsun000@gmail.com>
2025-05-10 23:59:20 +0900

    readme

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isProd ? '/resume-nextjs' : '',
-  assetPrefix: isProd ? '/resume-nextjs' : '',
+  assetPrefix: isProd ? '/resume-nextjs/' : '',
   images: {unoptimized: true}
 };
 
